9cad0fa3e634d2e49f211b901067be8160a8d871
Addikin <addik3n@gmail.com>
2025-06-18 23:21:30 -0500

    Final Iteration

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from stats import count_words, get_book_text, count_characters
+import sys
+from stats import count_words, get_book_text, char_counts, sorted_dict
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
     file_contents = get_book_text(filepath)
     word_count = count_words(file_contents)
-    print(f"{word_count} words found in the document")
-    character_contents = count_characters(file_contents)
-    print(character_contents)
-
+    character_contents = char_counts(file_contents)
+    final_dict = sorted_dict(character_contents)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}")
+    print(f"----------- Word Count -----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count ---------")
+    for dictionary in final_dict:
+        print(f"{dictionary['char']}: {dictionary['num']}")
+    print("============= END =============")
 main()
--- stats.py
+++ stats.py
@@ -5,7 +5,7 @@ def get_book_text(filepath):
 def count_words(file_contents):
         words = file_contents.split()
         return len(words)
-def count_characters(file_contents):
+def char_counts(file_contents):
     word_amounts = {}
     characters = file_contents.lower()
     for character in characters:
@@ -14,3 +14,13 @@ def count_characters(file_contents):
         else:
             word_amounts[character] += 1
     return word_amounts
+def sort_dict(dict):
+    return dict["num"]
+def sorted_dict(char_counts):
+    final_dict = []
+    for x in char_counts:
+        if x.isalpha():
+            before_final = {"char": x, "num": char_counts[x]}
+            final_dict.append(before_final)
+    final_dict.sort(reverse=True, key=sort_dict)
+    return final_dict
